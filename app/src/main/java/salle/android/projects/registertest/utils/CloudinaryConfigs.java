26742a2dfc0f82ea74ae44d400fc0ea20b5734d8
Carles Torrubiano <carles.torrubiano@students.salle.url.edu>
2020-03-23 11:04:40 +0100

    CloudinaryConfigs

---
+++ app/src/main/java/salle/android/projects/registertest/utils/CloudinaryConfigs.java
@@ -0,0 +1,14 @@
+package salle.android.projects.service_test.utils;
+
+import java.util.HashMap;
+
+public class CloudinaryConfigs {
+
+    public static HashMap getConfigurations() {
+        HashMap  config = new HashMap();
+        config.put("cloud_name", "carlestorru" );
+        config.put("api_key", "354843841544669");
+        config.put("api_secret", "8i87ojPM4tTbohZoy1bJM-DawMk");
+        return config;
+    }
+}
