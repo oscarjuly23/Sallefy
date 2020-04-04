a542d7e06b3790c47e59fb669a1654a92378fb8c
victorr97 <victorvalles97@gmail.com>
2020-04-04 19:32:54 +0200

    #31 Enviar cancion a Seekbar

--- app/src/main/java/salle/android/projects/registertest/controller/activity/MainActivity.java
+++ app/src/main/java/salle/android/projects/registertest/controller/activity/MainActivity.java
@@ -1,9 +1,19 @@
 package salle.android.projects.registertest.controller.activity;
 
 import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,30 +27,173 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
+import java.util.ArrayList;
+
 import salle.android.projects.registertest.R;
 import salle.android.projects.registertest.controller.callbacks.FragmentCallback;
 import salle.android.projects.registertest.controller.fragments.HomeFragment;
 import salle.android.projects.registertest.controller.fragments.SongsFragment;
+import salle.android.projects.registertest.controller.music.MusicCallback;
+import salle.android.projects.registertest.controller.music.MusicService;
+import salle.android.projects.registertest.model.Track;
 import salle.android.projects.registertest.utils.Constants;
 import salle.android.projects.registertest.utils.Session;
 
-public class MainActivity extends FragmentActivity implements FragmentCallback {
+public class MainActivity extends FragmentActivity implements FragmentCallback, MusicCallback {
 
     private FragmentManager mFragmentManager;
     private FragmentTransaction mTransaction;
 
     private BottomNavigationView mNav;
 
+    private static final String PLAY_VIEW = "PlayIcon";
+    private static final String STOP_VIEW = "StopIcon";
+
+    private TextView tvTitle;
+    private TextView tvAuthor;
+
+    private ImageButton btnBackward;
+    private ImageButton btnPlayStop;
+    private ImageButton btnForward;
+    private SeekBar mSeekBar;
+
+    private int mDuration;
+    private Handler mHandler;
+    private Runnable mRunnable;
+
+    // Service
+    private MusicService mBoundService;
+    private boolean mServiceBound = false;
+
+    private ArrayList<Track> mTracks;
+    private int currentTrack = 0;
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            mBoundService = binder.getService();
+            mBoundService.setCallback(MainActivity.this);
+            mServiceBound = true;
+            updateSeekBar();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+    };
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startStreamingService();
         initViews();
         setInitialFragment();
         requestPermissions();
     }
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mBoundService != null) {
+            resumeSongText();
+            if (mBoundService.isPlaying()) {
+                playAudio();
+            } else {
+                pauseAudio();
+            }
+        }
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mServiceBound) {
+            //pauseAudio();
+        }
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mServiceBound) {
+            this.unbindService(mServiceConnection);
+            mServiceBound = false;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
     private void initViews() {
+
+        mHandler = new Handler();
+
+        tvAuthor = findViewById(R.id.dynamic_artist);
+        tvTitle = findViewById(R.id.dynamic_title);
+
+        btnBackward = (ImageButton)findViewById(R.id.dynamic_backward_btn);
+        btnBackward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentTrack = ((currentTrack-1)%(mTracks.size()));
+                currentTrack = currentTrack < 0 ? (mTracks.size()-1):currentTrack;
+                updateTrack(currentTrack);
+            }
+        });
+        btnForward = (ImageButton)findViewById(R.id.dynamic_forward_btn);
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentTrack = ((currentTrack+1)%(mTracks.size()));
+                currentTrack = currentTrack >= mTracks.size() ? 0:currentTrack;
+                updateTrack(currentTrack);
+            }
+        });
+
+        btnPlayStop = (ImageButton)findViewById(R.id.dynamic_play_btn);
+        btnPlayStop.setTag(PLAY_VIEW);
+        btnPlayStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (btnPlayStop.getTag().equals(PLAY_VIEW)) {
+                    playAudio();
+                } else {
+                    pauseAudio();
+                }
+            }
+        });
+
+        mSeekBar = (SeekBar) findViewById(R.id.dynamic_seekBar);
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mBoundService.setCurrentDuration(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         mFragmentManager = getSupportFragmentManager();
         mTransaction = mFragmentManager.beginTransaction();
 
@@ -67,7 +220,65 @@ public class MainActivity extends FragmentActivity implements FragmentCallback {
                 return true;
             }
         });
+
     }
+
+        private void startStreamingService(){
+            Intent intent = new Intent(getApplicationContext(), MusicService.class);
+            this.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+        }
+
+        private void playAudio() {
+            if (!mBoundService.isPlaying()) { mBoundService.togglePlayer(); }
+            updateSeekBar();
+            btnPlayStop.setImageResource(R.drawable.ic_pause);
+            btnPlayStop.setTag(STOP_VIEW);
+            Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_SHORT).show();
+        }
+
+        private void pauseAudio() {
+            if (mBoundService.isPlaying()) { mBoundService.togglePlayer(); }
+            btnPlayStop.setImageResource(R.drawable.ic_play);
+            btnPlayStop.setTag(PLAY_VIEW);
+            Toast.makeText(getApplicationContext(), "Pausing Audio", Toast.LENGTH_SHORT).show();
+        }
+
+        public void updateSeekBar() {
+            System.out.println("max duration: " + mBoundService.getMaxDuration());
+            System.out.println("progress:" + mBoundService.getCurrrentPosition());
+            mSeekBar.setMax(mBoundService.getMaxDuration());
+            mSeekBar.setProgress(mBoundService.getCurrrentPosition());
+
+            if(mBoundService.isPlaying()) {
+                mRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        updateSeekBar();
+                    }
+                };
+                mHandler.postDelayed(mRunnable, 1000);
+            }
+        }
+
+    public void updateTrack(int index) {
+        Track track = mTracks.get(index);
+        currentTrack = index;
+        tvAuthor.setText(track.getUserLogin());
+        tvTitle.setText(track.getName());
+        mBoundService.playStream(mTracks, index);
+        btnPlayStop.setImageResource(R.drawable.ic_pause);
+        btnPlayStop.setTag(STOP_VIEW);
+        //updateSeekBar();
+    }
+
+    private void resumeSongText() {
+        Track track = mBoundService.getCurrentTrack();
+        if (track != null) {
+            tvAuthor.setText(track.getUserLogin());
+            tvTitle.setText(track.getName());
+        }
+    }
+
 
     private void setInitialFragment() {
         mTransaction.add(R.id.fragment_container, HomeFragment.getInstance());
@@ -149,4 +360,27 @@ public class MainActivity extends FragmentActivity implements FragmentCallback {
     }
 
 
+    @Override
+    public void onMusicPlayerPrepared() {
+        System.out.println("Entra en el prepared");
+        mSeekBar.setMax(mBoundService.getMaxDuration());
+        mDuration =  mBoundService.getMaxDuration();
+        playAudio();
+    }
+
+    public ArrayList<Track> getmTracks() {
+        return mTracks;
+    }
+
+    public void setmTracks(ArrayList<Track> mTracks) {
+        this.mTracks = mTracks;
+    }
+
+    public int getCurrentTrack() {
+        return currentTrack;
+    }
+
+    public void setCurrentTrack(int currentTrack) {
+        this.currentTrack = currentTrack;
+    }
 }
