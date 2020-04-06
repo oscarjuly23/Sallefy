de4c319efb362d39c4c882fc070b92f00b579d9b
victorr97 <victorvalles97@gmail.com>
2020-04-06 18:59:18 +0200

    #9 Show songs by genre

---
+++ app/src/main/java/salle/android/projects/registertest/controller/callbacks/GenreAdapterCallback.java
@@ -0,0 +1,7 @@
+package salle.android.projects.registertest.controller.callbacks;
+
+import salle.android.projects.registertest.model.Genre;
+
+public interface GenreAdapterCallback {
+    void onClickGenre(Genre genre);
+}
