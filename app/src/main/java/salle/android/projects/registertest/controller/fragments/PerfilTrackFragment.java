43fd817bda0b3d0a5ee856ee708c7babfeef73dc
Carles Torrubiano <carles.torrubiano@students.salle.url.edu>
2020-04-09 12:22:18 +0200

    #23 Mostrar mis canciones favoritas, reproducirlas, etc.

--- app/src/main/java/salle/android/projects/registertest/controller/fragments/PerfilTrackFragment.java
+++ app/src/main/java/salle/android/projects/registertest/controller/fragments/PerfilTrackFragment.java
@@ -10,15 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import salle.android.projects.registertest.R;
+import salle.android.projects.registertest.controller.adapters.TrackListAdapter;
 import salle.android.projects.registertest.controller.callbacks.FragmentCallback;
+import salle.android.projects.registertest.controller.callbacks.TrackListCallback;
+import salle.android.projects.registertest.model.Playlist;
 import salle.android.projects.registertest.model.Track;
+import salle.android.projects.registertest.restapi.callback.MeCallback;
+import salle.android.projects.registertest.restapi.manager.MeManager;
 
-public class PerfilTrackFragment extends Fragment implements FragmentCallback {
+public class PerfilTrackFragment extends Fragment implements FragmentCallback, MeCallback, TrackListCallback {
 
     public static final String TAG = PerfilTrackFragment.class.getName();
 
@@ -26,6 +33,7 @@ public class PerfilTrackFragment extends Fragment implements FragmentCallback {
     private RecyclerView mRecyclerView;
     private ArrayList<Track> mTracks;
     private int currentTrack = 0;
+    private FragmentCallback callback;
 
     public PerfilTrackFragment() {
     }
@@ -37,6 +45,7 @@ public class PerfilTrackFragment extends Fragment implements FragmentCallback {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        callback = (FragmentCallback) getActivity();
     }
 
     @Nullable
@@ -45,6 +54,7 @@ public class PerfilTrackFragment extends Fragment implements FragmentCallback {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_perfil_canciones, container, false);
         initViews(v);
+        getData();
         return v;
     }
 
@@ -54,11 +64,24 @@ public class PerfilTrackFragment extends Fragment implements FragmentCallback {
     }
 
     private void initViews(View v) {
-        /*mRecyclerView = (RecyclerView) v.findViewById(R.id.dynamic_recyclerView_playlists);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.dynamic_recyclerView_tracks);
         LinearLayoutManager manager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
         TrackListAdapter adapter = new TrackListAdapter(this, getActivity(), null);
         mRecyclerView.setLayoutManager(manager);
-        mRecyclerView.setAdapter(adapter);*/
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    private void getData() {
+        MeManager.getInstance(getActivity()).getMyTracksLiked(this);
+        mTracks = new ArrayList<>();
+    }
+
+    public int getIndex(){
+        return currentTrack;
+    }
+
+    public ArrayList<Track> getmTracks(){
+        return mTracks;
     }
 
     /**********************************************************************************************
@@ -76,5 +99,65 @@ public class PerfilTrackFragment extends Fragment implements FragmentCallback {
     @Override
     public void updateTrack(ArrayList<Track> mTracks, int index) {
         
+    }
+
+    /**********************************************************************************************
+     *   *   *   *   *   *   *   *   MeCallback   *   *   *   *   *   *   *   *   *
+     **********************************************************************************************/
+
+    @Override
+    public void myPlaylistsReceived(List<Playlist> playlists) {
+
+    }
+
+    @Override
+    public void playlistsFollowingReceived(List<Playlist> playlists) {
+
+    }
+
+    @Override
+    public void myTracksReceived(List<Track> tracks) {
+
+    }
+
+    @Override
+    public void tracksLikedReceived(List<Track> tracks) {
+        mTracks = (ArrayList) tracks;
+        TrackListAdapter adapter = new TrackListAdapter(this, getActivity(), mTracks);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void noPlaylistsReceived(Throwable throwable) {
+
+    }
+
+    @Override
+    public void noTracksReceived(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onFailure(Throwable throwable) {
+
+    }
+
+    /**********************************************************************************************
+     *   *   *   *   *   *   *   *   TrackListCallback   *   *   *   *   *   *   *   *   *
+     **********************************************************************************************/
+
+    @Override
+    public void onTrackSelected(Track track) {
+
+    }
+
+    @Override
+    public void onTrackSelected(int index) {
+        callback.updateTrack(mTracks, index);
+    }
+
+    @Override
+    public void onTrackLike(int index) {
+
     }
 }
